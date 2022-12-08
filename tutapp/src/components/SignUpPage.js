7749e69dfd9300b36aae0dd6b203a0b6e6d45715
bshekhar123 <bshekhar2001@gmail.com>
2022-12-08 17:29:27 +0530

    changes added

--- tutapp/src/components/SignUpPage.js
+++ tutapp/src/components/SignUpPage.js
@@ -47,7 +47,7 @@ const SignUpPage = () => {
                     <form className="space-y-6" action="post">
                         <div>
                             <label for="name" className="block mb-2 text-sm font-medium text-white">Your name</label>
-                            <input type="text" name="name" id="name" value={userdetails.name} onChange={changeHandler} className="border text-sm rounded-lg focus:ring-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder="Danishbir Singh" required />
+                            <input type="text" name="name" id="name" value={userdetails.name} onChange={changeHandler} className="border text-sm rounded-lg focus:ring-primary-600 block w-full p-2.5 bg-gray-700 border-gray-600 placeholder-gray-400 text-white focus:ring-blue-500 focus:border-blue-500" placeholder="Shekhar Biswas" required />
                         </div>
                         <div>
                             <label for="email" className="block mb-2 text-sm font-medium text-white">Your email</label>
